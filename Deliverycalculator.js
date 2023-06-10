96eaa66c15f24270aa7b3b211e7ce4f51b120885
MYSMARTLIFECA <136160724+MYSMARTLIFECA@users.noreply.github.com>
2023-06-10 11:09:07 -0400

    y

--- Deliverycalculator.js
+++ Deliverycalculator.js
@@ -46,17 +46,3 @@
 
     document.addEventListener('DOMContentLoaded', generateDeliveryTracker);
 
-
-//display message
-
-    var currentDate = new Date();
-var currentHour = currentDate.getHours();
-
-if (currentHour < 16) { // 4 PM is represented as 16 in the 24-hour format
-  var messageDivs = document.getElementsByClassName("message");
-  for (var i = 0; i < messageDivs.length; i++) {
-    var messageDiv = messageDivs[i];
-    messageDiv.textContent = "Get it before 11PM";
-    messageDiv.style.display = "block";
-  }
-}
