86d23b4afad38b27b211784c82715ab42f07f03d
MYSMARTLIFECA <136160724+MYSMARTLIFECA@users.noreply.github.com>
2023-06-10 13:54:33 -0400

    s

--- otheritems.js
+++ otheritems.js
@@ -1,48 +1,28 @@
-function generateDeliveryTrackerforotheritems() {
-    var deliveryTrackerHTML = `
-      <div class="delivery-container">
-        <div class="delivery-stage">
-          <i class="fas fa-shopping-cart"></i>
-          <h2 data-description="After you place your order, it will take 1-4 business days to prepare it for shipment.">Order placed</h2>
-          <p id="currentDate"></p>
-        </div>
-        <div class="delivery-stage">
-          <i class="fas fa-truck"></i>
-          <h2 data-description="MSL puts your order in the mail.">Order ships</h2>
-          <p id="datePlusThreeToFive"></p>
-        </div>
-        <div class="delivery-stage">
-          <i class="fas fa-check-circle"></i>
-          <h2 data-description="Estimated to arrive at your doorstep!">Delivered!</h2>
-          <p id="datePlusEightToEleven"></p>
-        </div>
-      </div>
-    `;
-
-    document.getElementById("otheritems").innerHTML = deliveryTrackerHTML;
-
-    var currentDate = new Date();
-
-    document.getElementById("currentDate").textContent = formatDate(currentDate);
-
-    var datePlusThreeToFive = new Date();
-    datePlusThreeToFive.setDate(currentDate.getDate() + 4);
-    var datePlusFive = new Date();
-    datePlusFive.setDate(currentDate.getDate() + 5);
-    document.getElementById("datePlusThreeToFive").textContent = formatDate(datePlusThreeToFive) + " - " + formatDate(datePlusFive);
-
-    var datePlusEightToEleven = new Date();
-    datePlusEightToEleven.setDate(currentDate.getDate() + 6);
-    var datePlusEleven = new Date();
-    datePlusEleven.setDate(currentDate.getDate() + 15);
-    document.getElementById("datePlusEightToEleven").textContent = formatDate(datePlusEightToEleven) + " - " + formatDate(datePlusEleven);
-
-    function formatDate(date) {
-      var month = date.toLocaleString('default', { month: 'short' });
-      var day = date.getDate();
-      return month + " " + day;
-    }
+ // Function to calculate the time
+ function calculateTime() {
+  var currentDate = new Date(); // Get the current date
+  var futureDate1 = new Date(); // Create a future date object for the first estimate
+  var futureDate2 = new Date(); // Create a future date object for the second estimate
+  
+  // Add 4 days to the future date 1
+  futureDate1.setDate(futureDate1.getDate() + 6);
+  
+  // Add 11 days to the future date 2
+  futureDate2.setDate(futureDate2.getDate() + 18);
+  
+  // Format the result as "MMM DD" string
+  var result1 = futureDate1.toLocaleString('default', { month: 'short', day: 'numeric' });
+  var result2 = futureDate2.toLocaleString('default', { month: 'short', day: 'numeric' });
+  
+  // Get all the elements with the class "timeDisplay"
+  var timeDisplays = document.getElementsByClassName("timeDisplay");
+  
+  // Loop through each element and set the inner text
+  for (var i = 0; i < timeDisplays.length; i++) {
+    timeDisplays[i].innerText = "Estimated Delivery:"+ "\n" + result1 + " - " + result2;
   }
+}
 
-  document.addEventListener('DOMContentLoaded', generateDeliveryTrackerforotheritems);
+// Call the calculateTime function when the page loads
+calculateTime();
 
