58a93f88108bc76dace1a1cf76f6026c7af60124
MYSMARTLIFECA <136160724+MYSMARTLIFECA@users.noreply.github.com>
2023-06-10 09:26:57 -0400

    Update before11pm.js

--- before11pm.js
+++ before11pm.js
@@ -1,8 +1,11 @@
-    var currentDate = new Date();
-    var currentHour = currentDate.getHours();
+var currentDate = new Date();
+var currentHour = currentDate.getHours();
 
-    if (currentHour < 16) { // 4 PM is represented as 16 in the 24-hour format
-      var messageDiv = document.getElementById("message");
-      messageDiv.innerText = "Get it before 11PM";
-      messageDiv.style.display = "block";
-    }
+if (currentHour < 16) { // 4 PM is represented as 16 in the 24-hour format
+  var messageDivs = document.getElementsByClassName("message");
+  for (var i = 0; i < messageDivs.length; i++) {
+    var messageDiv = messageDivs[i];
+    messageDiv.innerText = "Get it before 11PM";
+    messageDiv.style.display = "block";
+  }
+}
