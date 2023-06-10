0bf35e781be43286cbec9ebc0bbe00b8eae0c9b5
MYSMARTLIFECA <136160724+MYSMARTLIFECA@users.noreply.github.com>
2023-06-10 12:10:41 -0400

    t

--- same-day-delivery.js
+++ same-day-delivery.js
@@ -19,7 +19,7 @@ function generatesameDeliveryTracker() {
     </div>
   `;
 
-  document.getElementById("delivery-tracker-same-day").innerHTML = deliveryTrackerHTML;
+  document.getElementById("delivery-tracker").innerHTML = deliveryTrackerHTML;
 
   var currentDate = new Date();
   document.getElementById("currentDate").textContent = formatDate(currentDate);
@@ -51,3 +51,51 @@ function generatesameDeliveryTracker() {
 
 document.addEventListener("DOMContentLoaded", generatesameDeliveryTracker);
 
+
+function generateDeliveryTrackerExtended() {
+  var deliveryTrackerHTML = `
+    <div class="delivery-container">
+      <div class="delivery-stage">
+        <i class="fas fa-shopping-cart"></i>
+        <h2 data-description="After you place your order, it will take 1-3 business days to prepare it for shipment.">Order placed</h2>
+        <p id="currentDateExtended"></p>
+      </div>
+      <div class="delivery-stage">
+        <i class="fas fa-truck"></i>
+        <h2 data-description="MSL puts your order in the mail.">Order ships</h2>
+        <p id="datePlusThreeToFive"></p>
+      </div>
+      <div class="delivery-stage">
+        <i class="fas fa-check-circle"></i>
+        <h2 data-description="Estimated to arrive at your doorstep!">Delivered!</h2>
+        <p id="datePlusEightToEleven"></p>
+      </div>
+    </div>
+  `;
+
+  document.getElementById("delivery-tracker-extended").innerHTML = deliveryTrackerHTML;
+
+  var currentDate = new Date();
+
+  document.getElementById("currentDateExtended").textContent = formatDate(currentDate);
+
+  var datePlusThreeToFive = new Date();
+  datePlusThreeToFive.setDate(currentDate.getDate() + 3);
+  var datePlusFive = new Date();
+  datePlusFive.setDate(currentDate.getDate() + 5);
+  document.getElementById("datePlusThreeToFive").textContent = formatDate(datePlusThreeToFive) + " - " + formatDate(datePlusFive);
+
+  var datePlusEightToEleven = new Date();
+  datePlusEightToEleven.setDate(currentDate.getDate() + 6);
+  var datePlusEleven = new Date();
+  datePlusEleven.setDate(currentDate.getDate() + 12);
+  document.getElementById("datePlusEightToEleven").textContent = formatDate(datePlusEightToEleven) + " - " + formatDate(datePlusEleven);
+
+  function formatDate(date) {
+    var month = date.toLocaleString('default', { month: 'short' });
+    var day = date.getDate();
+    return month + " " + day;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', generateDeliveryTrackerExtended);
