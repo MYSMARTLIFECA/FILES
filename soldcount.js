2f2abce2d3079e2b7ac859a6719185580868a594
MYSMARTLIFECA <136160724+MYSMARTLIFECA@users.noreply.github.com>
2023-10-16 22:05:39 -0400

    s

--- soldcount.js
+++ soldcount.js
@@ -1,41 +1,54 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Function to generate a random number between 98 and 673
-    function getRandomNumber(min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-  
-    // Function to set a unique cookie for an element
-    function setElementCookie(element, value, days) {
-      const expires = new Date();
-      expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
-      document.cookie = `${element.id}=${value};expires=${expires.toUTCString()}`;
-    }
-  
-    // Function to get a unique cookie for an element
-    function getElementCookie(element) {
-      const cookies = document.cookie.split(';');
-      for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i].trim().split('=');
-        if (cookie[0] === element.id) {
-          return cookie[1];
-        }
-      }
-      return null;
-    }
-  
-    // Select all elements with the class "sold-count"
-    const soldElements = document.querySelectorAll('.sold-count');
-  
-    soldElements.forEach(function(element) {
-      // Generate a unique ID for the element
-      element.id = 'element_' + Date.now() + '_' + Math.floor(Math.random() * 1000);
-  
-      // Get the existing value from the unique cookie, or generate a new one
-      const existingValue = getElementCookie(element);
-      const randomValue = existingValue || getRandomNumber(98, 673);
-  
-      // Set the content and store it in a unique cookie
+  // Function to generate a random number between 98 and 673
+  function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  // Function to set a single cookie for all elements
+  function setElementCookies(elements) {
+    const elementData = {};
+    elements.forEach(element => {
+      const randomValue = getRandomNumber(98, 673);
+      elementData[element.id] = randomValue;
       element.textContent = `(${randomValue}+ sold)`;
-      setElementCookie(element, randomValue, 30); // Store for 30 days
     });
+
+    const cookieValue = JSON.stringify(elementData);
+    const expires = new Date();
+    expires.setTime(expires.getTime() + 30 * 24 * 60 * 60 * 1000); // Store for 30 days
+    document.cookie = `elementData=${cookieValue};expires=${expires.toUTCString()}`;
+  }
+
+  // Function to get the single cookie containing element information
+  function getElementCookies() {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim().split('=');
+      if (cookie[0] === 'elementData') {
+        return JSON.parse(cookie[1]);
+      }
+    }
+    return {};
+  }
+
+  // Select all elements with the class "sold-count"
+  const soldElements = document.querySelectorAll('.sold-count');
+
+  soldElements.forEach(function(element) {
+    // Generate a unique ID for the element
+    element.id = 'element_' + Date.now() + '_' + Math.floor(Math.random() * 1000);
   });
+
+  // Get element data from the cookie or generate new data
+  const elementData = getElementCookies();
+
+  soldElements.forEach(function(element) {
+    const existingValue = elementData[element.id];
+    const randomValue = existingValue || getRandomNumber(98, 673);
+    element.textContent = `(${randomValue}+ sold)`;
+    elementData[element.id] = randomValue;
+  });
+
+  // Update the cookie with the latest data
+  setElementCookies(soldElements);
+});
