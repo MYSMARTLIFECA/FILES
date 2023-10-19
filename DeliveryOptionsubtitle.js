b953fef1a14ff87bd593d80692c7231c026b9108
MYSMARTLIFECA <136160724+MYSMARTLIFECA@users.noreply.github.com>
2023-10-19 09:19:02 -0400

    dasd

--- DeliveryOptionsubtitle.js
+++ DeliveryOptionsubtitle.js
@@ -1,41 +1,49 @@
-function generateDeliveryTracker(deliveryType, preparationDays, shippingDays, deliveryDays) {
+//for same day delivery
+
+
+function generatesameDeliveryTracker() {
   var deliveryTrackerHTML = `
     <div class="delivery-container">
       <div class="delivery-stage">
         <i class="fas fa-shopping-cart"></i>
-        <h2 data-description="After you place your order, it will take ${preparationDays} business days to prepare it for shipment.">Order placed</h2>
+        <h2 data-description="After you place your order, it will be prepared for same-day shipment.">Order placed</h2>
         <p id="currentDate"></p>
       </div>
       <div class="delivery-stage">
         <i class="fas fa-truck"></i>
-        <h2 data-description="MSL puts your order in the mail.">Order ships</h2>
+        <h2 data-description="MSL ships your order for same-day delivery.">Order ships</h2>
         <p id="orderShipsDate"></p>
       </div>
       <div class="delivery-stage">
         <i class="fas fa-check-circle"></i>
-        <h2 data-description="Estimated to arrive at your doorstep!">Delivered!</h2>
+        <h2 data-description="Estimated to arrive at your doorstep today or tomorrow!">Delivered!</h2>
         <p id="deliveryDate"></p>
       </div>
     </div>
   `;
 
+  document.getElementById("delivery-tracker").innerHTML = deliveryTrackerHTML;
+
   var currentDate = new Date();
-  var currentElement = document.getElementById("currentDate");
-  var orderShipsElement = document.getElementById("orderShipsDate");
-  var deliveryDateElement = document.getElementById("deliveryDate");
+  document.getElementById("currentDate").textContent = formatDate(currentDate);
 
-  document.getElementById(deliveryType).innerHTML = deliveryTrackerHTML;
-  currentElement.textContent = formatDate(currentDate); 
+  var cutOffTime = new Date();
+  cutOffTime.setHours(16, 0, 0); // Set the cutoff time to 4 PM
 
-  var orderShipsDate = new Date();
-  orderShipsDate.setDate(currentDate.getDate() + preparationDays);
-  orderShipsElement.textContent = formatDate(orderShipsDate);
+  var orderShipsDate = "Today";
+  document.getElementById("orderShipsDate").textContent = orderShipsDate;
 
-  var deliveryTime = formatDate(orderShipsDate);
-  var deliveryDate = new Date(orderShipsDate);
-  deliveryDate.setDate(orderShipsDate.getDate() + shippingDays);
-  deliveryTime += " - " + formatDate(deliveryDate);
-  deliveryDateElement.textContent = deliveryTime;
+  var deliveryTime = "Before 11PM";
+  if (currentDate >= cutOffTime) {
+    // If the current time is after or at the cutoff, set the delivery time to tomorrow
+    var tomorrowDate = new Date(currentDate);
+    tomorrowDate.setDate(currentDate.getDate() + 1);
+    deliveryTime = "Tomorrow";
+    if (tomorrowDate.getDay() === currentDate.getDay()) {
+      deliveryTime = "Tomorrow (8pm-11pm)";
+    }
+  }
+  document.getElementById("deliveryDate").textContent = deliveryTime;
 
   function formatDate(date) {
     var month = date.toLocaleString("default", { month: "short" });
@@ -44,8 +52,110 @@ function generateDeliveryTracker(deliveryType, preparationDays, shippingDays, de
   }
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  generateDeliveryTracker("delivery-tracker", 0, 0, 0); // For same-day delivery
-  generateDeliveryTracker("delivery-tracker-extended", 1, 3, 8); // For normal delivery
-  generateDeliveryTracker("otheritems", 1, 4, 18); // For other items
-});
+document.addEventListener("DOMContentLoaded", generatesameDeliveryTracker);
+
+
+
+// for normal delivery
+
+
+function generateDeliveryTrackerExtended() {
+  var deliveryTrackerHTML = `
+    <div class="delivery-container">
+      <div class="delivery-stage">
+        <i class="fas fa-shopping-cart"></i>
+        <h2 data-description="After you place your order, it will take 1-3 business days to prepare it for shipment.">Order placed</h2>
+        <p id="currentDateExtended"></p>
+      </div>
+      <div class="delivery-stage">
+        <i class="fas fa-truck"></i>
+        <h2 data-description="MSL puts your order in the mail.">Order ships</h2>
+        <p id="datePlusThreeToFive"></p>
+      </div>
+      <div class="delivery-stage">
+        <i class="fas fa-check-circle"></i>
+        <h2 data-description="Estimated to arrive at your doorstep!">Delivered!</h2>
+        <p id="datePlusEightToEleven"></p>
+      </div>
+    </div>
+  `;
+
+
+  document.getElementById("delivery-tracker-extended").innerHTML = deliveryTrackerHTML;
+
+  var currentDate = new Date();
+
+  document.getElementById("currentDateExtended").textContent = formatDate(currentDate);
+
+  var datePlusThreeToFive = new Date();
+  datePlusThreeToFive.setDate(currentDate.getDate() + 1);
+  var datePlusFive = new Date();
+  datePlusFive.setDate(currentDate.getDate() + 4);
+  document.getElementById("datePlusThreeToFive").textContent = formatDate(datePlusThreeToFive) + " - " + formatDate(datePlusFive);
+
+  var datePlusEightToEleven = new Date();
+  datePlusEightToEleven.setDate(currentDate.getDate() + 6);
+  var datePlusEleven = new Date();
+  datePlusEleven.setDate(currentDate.getDate() + 12);
+  document.getElementById("datePlusEightToEleven").textContent = formatDate(datePlusEightToEleven) + " - " + formatDate(datePlusEleven);
+
+  function formatDate(date) {
+    var month = date.toLocaleString('default', { month: 'short' });
+    var day = date.getDate();
+    return month + " " + day;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', generateDeliveryTrackerExtended);
+
+
+//other items
+
+
+function generateDeliveryTrackerforotheritems() {
+  var deliveryTrackerHTML = `
+    <div class="delivery-container">
+      <div class="delivery-stage">
+        <i class="fas fa-shopping-cart"></i>
+        <h2 data-description="After you place your order, it will take 1-4 business days to prepare it for shipment.">Order placed</h2>
+        <p id="currentDate"></p>
+      </div>
+      <div class="delivery-stage">
+        <i class="fas fa-truck"></i>
+        <h2 data-description="MSL puts your order in the mail.">Order ships</h2>
+        <p id="datePlusThreeToFive"></p>
+      </div>
+      <div class="delivery-stage">
+        <i class="fas fa-check-circle"></i>
+        <h2 data-description="Estimated to arrive at your doorstep!">Delivered!</h2>
+        <p id="datePlusEightToEleven"></p>
+      </div>
+    </div>
+  `;
+
+  document.getElementById("otheritems").innerHTML = deliveryTrackerHTML;
+
+  var currentDate = new Date();
+
+  document.getElementById("currentDate").textContent = formatDate(currentDate);
+
+  var datePlusThreeToFive = new Date();
+  datePlusThreeToFive.setDate(currentDate.getDate() + 1);
+  var datePlusFive = new Date();
+  datePlusFive.setDate(currentDate.getDate() + 4);
+  document.getElementById("datePlusThreeToFive").textContent = formatDate(datePlusThreeToFive) + " - " + formatDate(datePlusFive);
+
+  var datePlusEightToEleven = new Date();
+  datePlusEightToEleven.setDate(currentDate.getDate() + 6);
+  var datePlusEleven = new Date();
+  datePlusEleven.setDate(currentDate.getDate() + 18);
+  document.getElementById("datePlusEightToEleven").textContent = formatDate(datePlusEightToEleven) + " - " + formatDate(datePlusEleven);
+
+  function formatDate(date) {
+    var month = date.toLocaleString('default', { month: 'short' });
+    var day = date.getDate();
+    return month + " " + day;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', generateDeliveryTrackerforotheritems);
